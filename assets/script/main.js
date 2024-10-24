8ac8d73d9581fbfd9d3ce93617d35a98ace34873
yuzuchan05 <j.teee.05@gmail.com>
2024-10-24 01:51:35 -0700

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,24 +15,17 @@ var mySwiper = new Swiper(".swiper", {
       bulletClass: 'swiper-pagination-bullet',
       bulletActiveClass: 'swiper-pagination-bullet-active',
       renderBullet: function (index, className) {
-        // 画面幅に応じてページネーションのドット数を変更
-        var dotsNum = window.innerWidth <= 767 ? 3 : 3;  // SPでは6つ、PCでは2つ
-        if (index < dotsNum) {
-            return '<span class="' + className + '"></span>';
-        }
-        return '';
+        return '<span class="' + className + '"></span>';
     },
   },
   breakpoints: {
       768: {
           slidesPerView: 3,   // pc表示(768~1199px)時のスライド数
           slidesPerGroup: 1,
-          spaceBetween: 20,
-      },
-      1200: {                  // pc表示(1200px以上)
-          slidesPerView: 3,
-          slidesPerGroup: 1,
-          spaceBetween: 20,
+          spaceBetween: 12,
+          pagination: {
+              el:'',
+          },
       },
   },
 });
