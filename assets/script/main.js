08e66d5a08abc4cdd6c58f4f316f3db0f03505ae
yuzuchan05 <j.teee.05@gmail.com>
2024-11-08 16:34:11 -0800

    修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,31 +1,31 @@
-/* 手動slider
+/* お客様の声スライダー
 -----------------------------------------*/
 var mySwiper = new Swiper(".swiper", {
-  loop: true,
-  speed: 1000,
-  slidesPerView: 1,  // SP表示時のスライド数
-  slidesPerGroup: 1,
-  spaceBetween: 15,
-  centeredSlides: false, // スライドを中央揃えしない
-  watchOverflow: true, // スライド数が少ない場合に無限ループを防ぐ
+    loop: true,
+    speed: 1000,
+    slidesPerView: 1,  // SP表示時のスライド数
+    slidesPerGroup: 1,
+    spaceBetween: 15,
+    centeredSlides: false, // スライドを中央揃えしない
+    watchOverflow: true, // スライド数が少ない場合に無限ループを防ぐ
 
-  pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      bulletClass: 'swiper-pagination-bullet',
-      bulletActiveClass: 'swiper-pagination-bullet-active',
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '"></span>';
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletClass: 'swiper-pagination-bullet',
+        bulletActiveClass: 'swiper-pagination-bullet-active',
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '"></span>';
+        },
     },
-  },
-  breakpoints: {
-      768: {
-          slidesPerView: 3,   // pc表示(768~1199px)時のスライド数
-          slidesPerGroup: 1,
-          spaceBetween: 12,
-          pagination: {
-              el: null,
-          },
-      },
-  },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,   // pc表示(768~1199px)時のスライド数
+            slidesPerGroup: 1,
+            spaceBetween: 12,
+            pagination: {
+                el: null,
+            },
+        },
+    },
 });
